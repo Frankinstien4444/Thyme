fc97dbbb6c4c9d58adc0b87dc942747048525ae5
Frankinstien4444 <55277259+Frankinstien4444@users.noreply.github.com>
2019-09-13 08:18:03 -0700

    Add files via upload

---
+++ Enums/JoinTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thyme.Enums
+{
+    public enum JoinTypes
+    {
+        none,
+        on,
+        inner_join,
+        left_join,
+        right_join,
+        outer_join,
+        and,
+        or
+    }
+}
